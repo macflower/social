fe1b961d51f4e967f4b80ef38a6a6df925585992
Andy Baiduganov <stonemm21074mm@gmail.com>
2022-08-29 19:17:36 +0500

    first commit
    first commit

---
+++ social/o.php
@@ -0,0 +1,12 @@
+composer require laravel-lang/lang:~7.0
+
+alias sail='bash vendor/bin/sail'
+
+sail npm run dev
+
+sail composer require laravel-lang/lang:~8.0
+
+gitgub.com/vladneverov/social-network-laravel
+
+Login with either email or username with Fortify Laravel 8
+
